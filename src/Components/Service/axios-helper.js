817aeed31691cf99015d34f7c5e78081e56d3228
faizanahmed27 <faizanahmed2710@gmail.com>
2025-11-16 13:32:14 +0530

    add space

--- src/Components/Service/axios-helper.js
+++ src/Components/Service/axios-helper.js
@@ -3,6 +3,7 @@ import axios from "axios";
 //export const BASE_URL = "http://localhost:8080";
 export const BASE_URL = "http://192.168.1.100:8080";
 
+
 export const http = axios.create({
    baseURL:BASE_URL 
 })
